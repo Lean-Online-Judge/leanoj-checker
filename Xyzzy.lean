3661f46815b6de3808cf2b6124274ee3b60f9f24
ansar <azhdarov.ansar@gmail.com>
2026-02-03 23:11:33 +0100

    added xyzzy

---
+++ Xyzzy.lean
@@ -0,0 +1,49 @@
+import Lean.CoreM
+import Lean.Replay
+import Lean.Environment
+import Lean.Util.CollectAxioms
+
+def main (args : List String) : IO UInt32 := do
+  let pref ← IO.currentDir
+  let LEAN_PATH : String := s!"{pref}/.lake/packages/batteries/.lake/build/lib/lean:{pref}/.lake/packages/Qq/.lake/build/lib/lean:{pref}/.lake/packages/aesop/.lake/build/lib/lean:{pref}/.lake/packages/proofwidgets/.lake/build/lib/lean:{pref}/.lake/packages/importGraph/.lake/build/lib/lean:{pref}/.lake/packages/mathlib/.lake/build/lib/lean:{pref}/.lake/packages/plausible/.lake/build/lib/lean:{pref}/.lake/packages/LeanSearchClient/.lake/build/lib/lean:"
+
+  let child ← IO.Process.spawn {
+    cmd := "lean"
+    args := #[args[1]!, "-o", "submission.olean"]
+    cwd := args[0]!
+    env := #[⟨"LEAN_PATH", LEAN_PATH⟩]
+  }
+
+  let submExitCode ← child.wait
+  if submExitCode ≠ 0 then
+    IO.println "compilation error"
+    return 44
+
+  Lean.initSearchPath (← Lean.findSysroot) [args[0]!]
+
+  let (mod, _) ← Lean.readModuleData (System.FilePath.join args[0]! "submission.olean")
+  let (_, state) ← Lean.importModulesCore mod.imports |>.run
+  let submEnv ← Lean.finalizeImport state #[{module := `submission}] {} 0 true true
+
+  let mut newConstants := {}
+  for name in mod.constNames, ci in mod.constants do
+    newConstants := newConstants.insert name ci
+
+  let task ← IO.asTask (submEnv.replay newConstants)
+  match task.get with
+  | .error _ =>
+    IO.println "environment error"
+    return 45
+  | .ok submEnv =>
+    match submEnv.find? `solution with
+    | none =>
+      IO.println "solution not found"
+      return 46
+    | some sol =>
+      let (_, state) := ((Lean.CollectAxioms.collect sol.name).run submEnv).run {}
+      for ax in state.axioms do
+        if ax ∉ [`propext, `Quot.sound, `Classical.choice] then
+          IO.println "forbidden axiom"
+          return 48
+      IO.println ":tada"
+      return 42
